691b7a6b183a28b1a99b1646f141985fc82e2668
unknown <r0660660@student.thomasmore.be>
2018-11-20 10:46:22 +0100

    Front end - shop items

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,27 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import {User} from '../interfaces/user';
 import {AuthService} from '../services/auth.service';
 import {RewardService} from '../services/reward.service';
+import {Reward} from '../interfaces/reward';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styles: []
+  styles: ['.capitalize {\n' +
+  '   text-transform: capitalize;\n' +
+  '}']
 })
 export class ShopComponent implements OnInit {
 
   user: User;
-  test: any;
-
-  rewards = [{
-    id: '1',
-    naam: 'Een bak Cara Pils',
-    punten: '10'
-  },
-  {
-    id: '2',
-    naam: 'Oculus Rift',
-    punten: '500'
-  }];
+  rewards: Reward;
 
   constructor(public authService: AuthService, public rewardService: RewardService) { }
 
