06828c96b7a0739926787602f2e65e06a7bd58f1
LauwersJ <r0660660@student.thomasmore.be>
2018-11-23 14:45:13 +0100

    Front end - 403 & 404

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styles: ['.center { position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto;}']
+  styleUrls: ['../permission/permission.component.scss']
 })
 export class ErrorComponent implements OnInit {
 
