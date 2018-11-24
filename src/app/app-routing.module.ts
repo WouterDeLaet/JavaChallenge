7f02577d30357756d3c88154bb1eae07fe35bc93
LauwersJ <r0660660@student.thomasmore.be>
2018-11-24 15:49:49 +0100

    Front end - dashboards, nav & style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {ErrorComponent} from './error/error.component';
 import {LoginComponent} from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {RewardFormComponent} from './reward-form/reward-form.component';
-import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
+import {DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 import {PermissionComponent} from './permission/permission.component';
 import {OpdrachtGoedKeurenComponent} from './opdracht-goed-keuren/opdracht-goed-keuren.component';
 import {AuthGuard} from './guards/auth.guard';
